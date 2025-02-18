1bc17a3978ab260f56433dc02b7cfafe1252cb7a
GEZA GEOVANNI MERCZ ARELLANO <22151197@aguascalientes.tecnm.mx>
2025-02-18 16:53:38 -0600

    Create the TopBar of Nike App and create the part of genders

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.workclass.ui.screens.HomeScreen
+import com.example.workclass.ui.screens.InterfazNike
 import com.example.workclass.ui.screens.MainMenuScreen
 import com.example.workclass.ui.screens.TestScreen
 import com.example.workclass.ui.theme.WorkClassTheme
@@ -67,10 +68,12 @@ fun ComposeMultiScreenApp() { //La navegacion entre pantallas
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) { //Es el que nos va mandar a la pantalla del menu que vamos a querer por medio de una ruta
-    NavHost(navController = navController, startDestination = "main_menu") {
+    NavHost(navController = navController, startDestination = "interface_screen") {
         composable("main_menu") { MainMenuScreen(navController) }
         composable("home_screen") { HomeScreen(navController) }
         composable("test_screen") { TestScreen(navController) }
+        composable("interface_screen") { InterfazNike(navController) }
+
 
 
     }
---
+++ app/src/main/java/com/example/workclass/ui/screens/InterfazNike.kt
@@ -0,0 +1,135 @@
+package com.example.workclass.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.workclass.R
+
+
+@Composable
+fun InterfazNike(navController: NavHostController){
+    Column(
+        modifier = Modifier
+            .padding(0.dp,15.dp,0.dp,0.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        TopBarNike()
+        BarraCat()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarNike(){
+    Column {
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp)
+
+        ){
+            Icon(
+                Icons.Filled.Menu,
+                contentDescription = "Menu Icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(22.dp)
+            )
+            Text(
+                text = "Comprar",
+                color = Color.Black,
+                fontSize = 17.sp,
+                modifier = Modifier
+                    .padding(30.dp,0.dp,170.dp,0.dp)
+
+
+            )
+            Icon(
+                Icons.Filled.Search,
+                contentDescription = "Search",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(20.dp)
+                    .padding(0.dp,0.dp,1.dp,0.dp)
+            )
+            Icon(
+                Icons.Filled.ShoppingCart,
+                contentDescription = "Menu Icon",
+                tint = Color.Black,
+
+                modifier = Modifier
+                    .size(20.dp)
+
+            )
+
+
+
+        }
+
+
+    }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun BarraCat(){
+    Column {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp,2.dp,0.dp,0.dp)
+        ){
+            Text(
+                stringResource(R.string.Men),
+                color = Color.Black,
+                fontSize = 17.sp,
+                modifier = Modifier
+                    .padding(30.dp,0.dp,0.dp,0.dp)
+            )
+            Row {
+                Text(
+                    stringResource(R.string.Woman),
+                    color = Color.Black,
+                    fontSize = 17.sp,
+                    modifier = Modifier
+                        .padding(30.dp,0.dp,0.dp,30.dp)
+
+
+                )
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
