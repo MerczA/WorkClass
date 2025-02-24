84b789f4d81f33e380fa037170e0e77f53fddb0f
mercz <22151197@aguascalientes.tecnm.mx>
2025-02-24 17:28:43 -0600

    Create the File ComponentScreen.kt and added 2 types of Progress bar Linear and Circular

--- app/src/main/java/com/example/workclass/ui/screens/ComponentScreen.kt
+++ app/src/main/java/com/example/workclass/ui/screens/ComponentScreen.kt
@@ -3,15 +3,19 @@ package com.example.workclass.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
@@ -20,13 +24,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun ComponentScreen(navController: NavHostController){
 //buttons()
-    FloatingButtons()
+    //FloatingButtons()
+    Progress()
 
 }
 //@Preview(showBackground = true)
@@ -56,7 +62,7 @@ fun buttons(){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun FloatingButtons(){
     Column(
@@ -94,3 +100,24 @@ fun FloatingButtons(){
 
     }
     }
+
+@Preview(showBackground = true)
+@Composable
+fun Progress() { //Indicadores de progreso
+    Column(
+        modifier = Modifier
+            .fillMaxSize(), //Para que ocupe el 100% de mi pantalla
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+        )
+
+    }
+}
