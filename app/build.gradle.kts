210c0cc59bdcb3d7ef8bd5c56090d99265b2c9d4
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-07 07:53:37 -0600

    Add dependecias Internal Storage with Room and creation files of local DataBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -81,6 +84,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")*/
+
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
 
 
 
---
+++ app/src/main/java/com/example/workclass/data/database/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.workclass.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.workclass.data.dao.AccountDao
+import com.example.workclass.data.model.AccountEntity
+
+@Database(entities = [AccountEntity::class], version = 1)
+abstract class AppDatabase:RoomDatabase(){
+    abstract fun accountDao(): AccountDao
+}
